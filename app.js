599722167221f97e64351fde7f1711aae2d827c7
SravyaAbburi <abburi.sravya@gmail.com>
2022-09-30 19:28:50 +0530

    hashing passwords

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const dotenv=require("dotenv");
-const encrypt=require("mongoose-encryption");
+
+const md5 = require('md5');
 
 dotenv.config();
 
@@ -21,7 +22,6 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['password']  });
 
 const User=new mongoose.model("User",userSchema);
 
@@ -45,7 +45,7 @@ app.post("/register",function(req,res){
 
 const newUser=new User({
   email:req.body.username,
-  password:req.body.password
+  password:md5(req.body.password)
 });
 newUser.save(function(err){
   if(err){
@@ -61,7 +61,7 @@ newUser.save(function(err){
 
 app.post("/login",function(req,res){
 const username=req.body.username;
-const password=req.body.password;
+const password=md5(req.body.password);
 
 User.findOne({email:username},function(err,foundUser){
   if(err){
